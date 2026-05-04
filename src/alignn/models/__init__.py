054d813ace4d7b7dd2feed26de37d74d07856179
MohdShahulMalik <mohdshahulmalik@gmail.com>
2026-05-04 20:54:41 +0000

    feat(models): export ALIGNN and baseline modules

--- src/alignn/models/__init__.py
+++ src/alignn/models/__init__.py
@@ -1,1 +1,6 @@
 """Model modules for the ALIGNN reimplementation."""
+
+from alignn.models.alignn_model import ALIGNNLayer, ALIGNNModel
+from alignn.models.baseline_gnn import BaselineGNN, EdgeGatedGraphConv
+
+__all__ = ["ALIGNNLayer", "ALIGNNModel", "BaselineGNN", "EdgeGatedGraphConv"]
